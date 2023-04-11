e6040bb7097f287fb025f396a57f7de201d54379
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-04-11 10:28:56 +0300

    Remove redundant templates. Add todo comments to methods that need to be remade.

--- src/main/java/fi/haagahelia/surveytool/web/SurveyController.java
+++ src/main/java/fi/haagahelia/surveytool/web/SurveyController.java
@@ -1,25 +1,15 @@
 package fi.haagahelia.surveytool.web;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import fi.haagahelia.surveytool.domain.Question;
 import fi.haagahelia.surveytool.domain.QuestionRepository;
 import fi.haagahelia.surveytool.domain.Survey;
 import fi.haagahelia.surveytool.domain.SurveyRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 //@CrossOrigin (origins="http:/localhost:8080")
 @Controller
@@ -45,6 +35,7 @@ public class SurveyController {
 	
 	@GetMapping("/add-survey")
 	public String addNewSurvey(Model model){
+		//TODO: tämä uudestaan uuden 11.4 suunnitelman mukaan
 		model.addAttribute("survey", new Survey());
 		model.addAttribute("questions", questionRepository.findAll());
 		return "add-survey";
@@ -52,36 +43,10 @@ public class SurveyController {
 	
 	@PostMapping("/save-survey")
 	public String saveSurvey(Survey survey) {
+		//TODO: tämä uudestaan uuden 11.4 suunnitelman mukaan
 		surveyRepository.save(survey);
 		return "redirect:../surveys";
 	}
-	
-	/*
-	 * Yritys tehdä jotain hienoa
-	 * 
-	 * @Transactional
-	@PostMapping("/submit-survey")
-		public String submitSurvey(HttpServletRequest request, Model model){
-			String name = request.getParameter("name");
-			String[] questions = request.getParameterValues("questions");
-			Survey survey = new Survey();
-			survey.setName(name);
-			List <Question> questionList = new ArrayList<>();
-			for(String question : questions) {
-				Question newQuestion = new Question();
-				newQuestion.setSurvey(survey);
-				newQuestion.setContent(question);
-				questionList.add(newQuestion);
-			}
-			System.out.println(questions);
-			
-			survey.setQuestions(questionList);
-			System.out.println(survey.toString());
-			surveyRepository.save(survey);
-			return "redirect:../surveys";
-		}
-		
-	*/ 
 	
 	// REST api-call that gets all questions based in surveyId and returns them as JSON
 	@GetMapping("/surveys/{id}")
