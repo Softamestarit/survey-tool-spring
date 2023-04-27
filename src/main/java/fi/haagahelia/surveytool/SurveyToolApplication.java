ecab4feb6d023ec1bbf4993cf0937a1e75cce832
Ada Meriläinen <ada.merilainen@gmail.com>
2023-04-27 10:42:09 +0300

    added answer import to commandlinerunner

--- src/main/java/fi/haagahelia/surveytool/SurveyToolApplication.java
+++ src/main/java/fi/haagahelia/surveytool/SurveyToolApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import fi.haagahelia.surveytool.domain.Answer;
 import fi.haagahelia.surveytool.domain.AnswerRepository;
 import fi.haagahelia.surveytool.domain.Option;
 import fi.haagahelia.surveytool.domain.OptionRepository;
