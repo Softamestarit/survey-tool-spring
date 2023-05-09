a37f33427e3a0fbb45bd82a4bfef2eb1cf6a4cd7
Ada Meriläinen <ada.merilainen@gmail.com>
2023-05-09 09:48:07 +0300

    fixed survey-page.js

--- src/main/resources/static/js/survey-page-script.js
+++ src/main/resources/static/js/survey-page-script.js
@@ -4,12 +4,8 @@ window.addEventListener('load', function() {
 	const heading = document.getElementById("survey-title");
 	const content = document.getElementById("survey-description");
 	const editForm = document.getElementById("description-form");
-<<<<<<< HEAD:src/main/resources/static/js/editDescription.js
 	const editSurvey = document.getElementById("edit-title");
 	
-=======
-
->>>>>>> 2f60697d57de13bd2ae2976f9941ece45a5e5cf7:src/main/resources/static/js/survey-page-script.js
 	editButton.addEventListener("click", () => {
 		heading.style.display = "none";
 		content.style.display = "none";
